b0a9871c892cc1e16c6a7446b640d1de682ef828
BK <benkoshy@hotmail.com>
2022-01-27 13:03:00 +1100

    add: pagy.js via import maps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import React from "react"
+import Pagy from "pagy"
+
+console.log("hello world 1")
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# pin "pagy", to: "https://raw.githubusercontent.com/ddnexus/pagy/master/lib/javascripts/pagy-module.js"
+pin "pagy", to: Pagy.root.join('javascripts', 'pagy-module.js')
+pin "react", to: "https://cdn.skypack.dev/react" # test
